a7de968d447e35d6a12e6df8c91ea397b90a4c8b
Tofik Khan <tofik.khan@mkausa.org>
2023-01-07 21:18:36 -0800

    Establish connection to DB

---
+++ packages/mka-northwest/send-sms/main.js
@@ -0,0 +1,31 @@
+const {MongoClient} = require('mongodb');
+
+exports.main = async () => {
+    console.log("...Initiating Cron...");
+    
+    let khuddam = await getAllKhuddam();
+    console.log(khuddam);
+
+    return {"body": "Done!"}
+}
+
+
+async function getAllKhuddam() {
+    const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`;
+
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+
+        return await client
+                        .db("mka-northwest")
+                        .collection('khuddam')
+                        .find({})
+                        .toArray(); 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
